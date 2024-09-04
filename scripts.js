e3c5acfa266066e516d287176d5e5bd45db4789b
Salvador Silva <salvadorvilarsilva@gmail.com>
2024-09-04 10:44:12 +0100

    feat: add animation

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+//text typing animation
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed('#typed-text', {
+        strings: ['&lt; Backend Developer /&gt;', '&lt; Scala Enthusiast /&gt;'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        backDelay: 1500,
+        loop: true,
+    });
+});
+//animation
+document.addEventListener('DOMContentLoaded', function () {
+    var animationContainer = document.getElementById('lottie-animation');
+    var animation = lottie.loadAnimation({
+        container: animationContainer,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'Animations/Animation - 1725382294510.json'
+    });
+});
+
+//glow effect
+document.addEventListener('DOMContentLoaded', () => {
+    // Create a cursor element
+    const cursor = document.createElement('div');
+    cursor.classList.add('cursor');
+    document.body.appendChild(cursor);
+
+    // Move the cursor element with the mouse
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.left = `${e.pageX}px`;
+        cursor.style.top = `${e.pageY}px`;
+    });
+
+});
